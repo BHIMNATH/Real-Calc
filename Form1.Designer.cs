4803edd6a3e1402041a1e6e0edce66cca888e382
BHIMNATH <you@example.com>
2017-04-12 23:33:59 +0530

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class calcer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(calcer));
+            this.but0 = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.min = new System.Windows.Forms.Button();
+            this.star = new System.Windows.Forms.Button();
+            this.slash = new System.Windows.Forms.Button();
+            this.perc = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.calculate = new System.Windows.Forms.Button();
+            this.rese = new System.Windows.Forms.Button();
+            this.answ = new System.Windows.Forms.Label();
+            this.view = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // but0
+            // 
+            this.but0.Location = new System.Drawing.Point(21, 172);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(79, 36);
+            this.but0.TabIndex = 0;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(124, 172);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(78, 36);
+            this.plus.TabIndex = 0;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // min
+            // 
+            this.min.Location = new System.Drawing.Point(226, 172);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(75, 36);
+            this.min.TabIndex = 0;
+            this.min.Text = "-";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // star
+            // 
+            this.star.Location = new System.Drawing.Point(21, 225);
+            this.star.Name = "star";
+            this.star.Size = new System.Drawing.Size(79, 35);
+            this.star.TabIndex = 0;
+            this.star.Text = "*";
+            this.star.UseVisualStyleBackColor = true;
+            this.star.Click += new System.EventHandler(this.star_Click);
+            // 
+            // slash
+            // 
+            this.slash.Location = new System.Drawing.Point(124, 225);
+            this.slash.Name = "slash";
+            this.slash.Size = new System.Drawing.Size(78, 35);
+            this.slash.TabIndex = 0;
+            this.slash.Text = "/";
+            this.slash.UseVisualStyleBackColor = true;
+            this.slash.Click += new System.EventHandler(this.slash_Click);
+            // 
+            // perc
+            // 
+            this.perc.Location = new System.Drawing.Point(226, 225);
+            this.perc.Name = "perc";
+            this.perc.Size = new System.Drawing.Size(75, 35);
+            this.perc.TabIndex = 0;
+            this.perc.Text = "%";
+            this.perc.UseVisualStyleBackColor = true;
+            this.perc.Click += new System.EventHandler(this.perc_Click);
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(21, 12);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(79, 37);
+            this.but1.TabIndex = 0;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // but9
+            // 
+            this.but9.Location = new System.Drawing.Point(226, 118);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(75, 34);
+            this.but9.TabIndex = 0;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.but9_Click);
+            // 
+            // but6
+            // 
+            this.but6.Location = new System.Drawing.Point(226, 68);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(75, 36);
+            this.but6.TabIndex = 0;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.but6_Click);
+            // 
+            // but3
+            // 
+            this.but3.Location = new System.Drawing.Point(226, 12);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(75, 37);
+            this.but3.TabIndex = 0;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but8
+            // 
+            this.but8.Location = new System.Drawing.Point(124, 118);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(78, 34);
+            this.but8.TabIndex = 0;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.but8_Click);
+            // 
+            // but7
+            // 
+            this.but7.Location = new System.Drawing.Point(21, 118);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(79, 34);
+            this.but7.TabIndex = 0;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.but7_Click);
+            // 
+            // but5
+            // 
+            this.but5.Location = new System.Drawing.Point(124, 68);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(78, 36);
+            this.but5.TabIndex = 0;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.but5_Click);
+            // 
+            // but4
+            // 
+            this.but4.Location = new System.Drawing.Point(21, 68);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(79, 36);
+            this.but4.TabIndex = 0;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(124, 12);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(78, 37);
+            this.but2.TabIndex = 0;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 102F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 83F));
+            this.tableLayoutPanel1.Controls.Add(this.but2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.but4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.but5, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.but7, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.but8, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.but3, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.but6, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.but9, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.but1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.perc, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.slash, 2, 5);
+            this.tableLayoutPanel1.Controls.Add(this.star, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.min, 3, 4);
+            this.tableLayoutPanel1.Controls.Add(this.plus, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.but0, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(26, 62);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.7541F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.2459F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 52F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(307, 275);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(69, 352);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(113, 23);
+            this.calculate.TabIndex = 1;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // rese
+            // 
+            this.rese.Location = new System.Drawing.Point(188, 353);
+            this.rese.Name = "rese";
+            this.rese.Size = new System.Drawing.Size(125, 22);
+            this.rese.TabIndex = 3;
+            this.rese.Text = "Reset";
+            this.rese.UseVisualStyleBackColor = true;
+            this.rese.Click += new System.EventHandler(this.rese_Click);
+            // 
+            // answ
+            // 
+            this.answ.AutoSize = true;
+            this.answ.Location = new System.Drawing.Point(235, 26);
+            this.answ.Name = "answ";
+            this.answ.Size = new System.Drawing.Size(0, 13);
+            this.answ.TabIndex = 4;
+            // 
+            // view
+            // 
+            this.view.Location = new System.Drawing.Point(26, 23);
+            this.view.Name = "view";
+            this.view.ReadOnly = true;
+            this.view.Size = new System.Drawing.Size(307, 20);
+            this.view.TabIndex = 2;
+            this.view.TextChanged += new System.EventHandler(this.view_TextChanged);
+            // 
+            // calcer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 387);
+            this.Controls.Add(this.answ);
+            this.Controls.Add(this.rese);
+            this.Controls.Add(this.view);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(306, 345);
+            this.Name = "calcer";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Tag = "Aswin Bhim Nath";
+            this.Text = global::WindowsFormsApplication1.Properties.Settings.Default.Sw;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but0;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button star;
+        private System.Windows.Forms.Button slash;
+        private System.Windows.Forms.Button perc;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button rese;
+        private System.Windows.Forms.Label answ;
+        private System.Windows.Forms.TextBox view;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class calcer : Form
+    {
+        public calcer()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void but1_Click(object sender, EventArgs e)
+        {
+            view.Text =view.Text + but1.Text;
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but2.Text;
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but3.Text;
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but4.Text;
+        }
+
+        private void but5_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but5.Text;
+        }
+
+        private void but6_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but6.Text;
+        }
+
+        private void but7_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but7.Text;
+        }
+
+        private void but8_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but8.Text;
+        }
+
+        private void but9_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but9.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            view.Text = view.Text + but0.Text;
+        }
+
+       // double ch=0;
+        double a, b;
+        double c = 0;
+        int cout = 0;
+
+    }
+}
